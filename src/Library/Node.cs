e64f9ee3b0ab0ab5c1304f51ace8366579d0513d
Federico Ferreira <FEDERICO.FERREIRAF@correo.ucu.edu.uy>
2021-10-27 18:29:37 -0300

    Añado cambios a Node

--- src/Library/Node.cs
+++ src/Library/Node.cs
@@ -6,16 +6,10 @@ namespace Library
 {
     public class Node
     {
-        private int number;
 
+        private Persona persona;
+        public Persona Persona {get;}
         private List<Node> children = new List<Node>();
-
-        public int Number {
-            get
-            {
-                return this.number;
-            }
-        }
 
         public ReadOnlyCollection<Node> Children { 
             get
@@ -24,9 +18,9 @@ namespace Library
             }
         }
 
-        public Node(int number)
+        public Node(Persona persona)
         {
-            this.number = number;
+            this.persona = persona;
         }
 
         public void AddChildren(Node n)
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,13 +7,23 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Persona Uno = new Persona("Pedro Uno", 80);
+            Persona Dos = new Persona("Pedro Dos", 70);
+            Persona Tres = new Persona("Pedro Tres", 60);
+            Persona Cuatro = new Persona("Pedro Cuatro", 50);
+            Persona Cinco = new Persona("Pedro Cinco", 40);
+            Persona Seis = new Persona("Pedro Seis", 30);
+            Persona Siete = new Persona("Pedro Siete", 20);
+            Persona Ocho = new Persona("Pedro Ocho", 10);
+
+            Node n1 = new Node(Uno);
+            Node n2 = new Node(Dos);
+            Node n3 = new Node(Tres);
+            Node n4 = new Node(Cuatro);
+            Node n5 = new Node(Cinco);
+            Node n6 = new Node(Seis);
+            Node n7 = new Node(Siete);
+            Node n8 = new Node(Ocho);
 
             n1.AddChildren(n2);
             n1.AddChildren(n3);
@@ -23,6 +33,8 @@ namespace Program
 
             n3.AddChildren(n6);
             n3.AddChildren(n7);
+
+            n4.AddChildren(n8);
 
             // visitar el árbol aquí
         }
